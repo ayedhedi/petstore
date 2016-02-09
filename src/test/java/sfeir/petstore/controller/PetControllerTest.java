fb3df2bbf5ada055d9a01142bd73b71408fbd13e
ayed.h <ayed.h@sfei.lu>
2016-02-09 11:00:12 +0100

    adding first test

---
+++ src/test/java/sfeir/petstore/controller/PetControllerTest.java
@@ -0,0 +1,16 @@
+package sfeir.petstore.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ayed.h on 09/02/2016.
+ */
+public class PetControllerTest {
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+}
